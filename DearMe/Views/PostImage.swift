254409ff7fd0381edd48a280b984a406eb324f2f
MinwooJe <gpatration77@gmail.com>
2023-11-17 00:45:50 +0900

    feat: PostImageView, PostImageViewController 선언

---
+++ DearMe/Views/PostImage.swift
@@ -0,0 +1,20 @@
+//
+//  PostImage.swift
+//  DearMe
+//
+//  Created by 제민우 on 11/17/23.
+//
+
+import UIKit
+
+class PostImage: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
