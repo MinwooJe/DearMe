e2b1c38e6a3a2228459c07412dca144e7c3dc1e3
MinwooJe <gpatration77@gmail.com>
2023-11-17 14:55:57 +0900

    feat: UIImagePicker, PHPicker 권한 설정

--- DearMe/Controllers/PostImageViewController.swift
+++ DearMe/Controllers/PostImageViewController.swift
@@ -7,10 +7,15 @@
 
 import UIKit
 import PhotosUI
+import AVFoundation
 
 final class PostImageViewController: UIViewController {
     
     let postImageView = PostImageView()
+    
+    private var isCameraAuthorized: Bool {
+      AVCaptureDevice.authorizationStatus(for: .video) == .authorized
+    }
     
     override func loadView() {
         view = postImageView
@@ -86,6 +91,12 @@ extension PostImageViewController: PHPickerViewControllerDelegate {
 extension PostImageViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     private func openCamera() {
+        AVCaptureDevice.requestAccess(for: .video) { [weak self] isAuthorized in
+            guard isAuthorized else {
+                self?.showAlertGoToSetting()
+                return
+            }
+        }
         if(UIImagePickerController.isSourceTypeAvailable(.camera)) {
             let addPostImagePicker = UIImagePickerController()
             
@@ -103,5 +114,33 @@ extension PostImageViewController: UIImagePickerControllerDelegate, UINavigation
             postImageView.cameraImageView.image = image
             dismiss(animated: true)
         }
+    }
+    
+    func showAlertGoToSetting() {
+      let alertController = UIAlertController(
+        title: "현재 카메라 사용에 대한 접근 권한이 없습니다.",
+        message: "설정 > {앱 이름}탭에서 접근을 활성화 할 수 있습니다.",
+        preferredStyle: .alert
+      )
+      let cancelAlert = UIAlertAction(
+        title: "취소",
+        style: .cancel
+      ) { _ in
+          alertController.dismiss(animated: true, completion: nil)
+        }
+      let goToSettingAlert = UIAlertAction(
+        title: "설정으로 이동하기",
+        style: .default) { _ in
+          guard
+            let settingURL = URL(string: UIApplication.openSettingsURLString),
+            UIApplication.shared.canOpenURL(settingURL)
+          else { return }
+          UIApplication.shared.open(settingURL, options: [:])
+        }
+      [cancelAlert, goToSettingAlert]
+        .forEach(alertController.addAction(_:))
+      DispatchQueue.main.async {
+        self.present(alertController, animated: true) // must be used from main thread only
+      }
     }
 }
