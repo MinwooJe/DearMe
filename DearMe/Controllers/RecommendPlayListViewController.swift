0c218af97759c100f45ef9cc56298ae980fdd641
MinwooJe <gpatration77@gmail.com>
2023-11-19 01:55:09 +0900

    feat: RecommendPlayListViewController 선언

---
+++ DearMe/Controllers/RecommendPlayListViewController.swift
@@ -0,0 +1,15 @@
+//
+//  RecommendPlayListViewController.swift
+//  DearMe
+//
+//  Created by 제민우 on 11/19/23.
+//
+
+import UIKit
+
+final class RecommendPlayListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
