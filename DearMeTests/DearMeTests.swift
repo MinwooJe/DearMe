c96fe01ba2b495fe5d11f3ae9ac6a62a4e7830ec
MinwooJe <gpatration77@gmail.com>
2024-11-13 15:47:43 +0900

    chore: 단위테스트 셋팅

---
+++ DearMeTests/DearMeTests.swift
@@ -0,0 +1,26 @@
+//
+//  DearMeTests.swift
+//  DearMeTests
+//
+//  Created by 제민우 on 11/13/24.
+//
+
+import XCTest
+
+final class DearMeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    func testExample() throws {
+        XCTAssert(true)
+    }
+    
+//    func test_무조건_실패하는_테스트() throws {
+//        XCTAssert(false)
+//    }
+
+}
